b55951068df565e1981d9d4a75f2d65ef5671030
Zakarye R. <168565323+zared1@users.noreply.github.com>
2025-09-06 16:13:45 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+server_address = ('localhost', 10000)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(server_address)
+sock.listen()
+
+def handle_client(conn, address):
+    data = conn.recv(4096)
+    data = data.decode()
+    print(f'Received {data} from {address}')
+
+def handle_incoming_connections():
+    while True:
+        conn, address = sock.accept()
+        print(f'Accepted connection from {address}')
+
+        thread = threading.Thread(target=handle_client, args=(conn, address))
+        thread.start()
+
+thread = threading.Thread(target=handle_incoming_connections)
+thread.start()
+
+while True:
+    message = input('Enter a message to send: ')
+    for conn, address in connected_clients:
+        conn.send(message.encode())
